f0184528ce64551aeb3cb71b105e54ac4c4326bf
Amed Kino <amed@xbyte.team>
2016-11-07 12:38:06 +0100

    add design, first step

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Gotin
   class Application < Rails::Application
+  config.alphabit = ActiveSupport::OrderedOptions.new
+  config.alphabit.capital = ["A", "B", "C", "Ç", "D", "E", "Ê", "F", "G", "H", "I", "Î", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "Ş", "T", "U", "Û", "V", "W", "X", "Y", "Z"]
+  # config.i18n.default_locale = :ku
+
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
